29e3bfc58015c9c5f4e9d5cfbb6d78b66aba1528
Florian <courrierdsf@gmail.com>
2025-02-28 19:48:19 +0100

    TP4 - fix start_workers.bat

--- TP4_MasterWorker/src/start_workers.bat
+++ TP4_MasterWorker/src/start_workers.bat
@@ -1,5 +1,7 @@
 @echo off
+setlocal enabledelayedexpansion
 REM Chemin vers l'exécutable Java, A modifier en fonction du poste (Java n'est pas déclaré dans mes variables d'environnement et ca peut arriver sur un autre poste donc whatever)set JAVA_EXE="C:\Users\IAN78\.jdks\openjdk-19.0.2-7\bin\java.exe"
+set JAVA_EXE="C:\Users\IAN78\.jdks\openjdk-19.0.2-7\bin\java.exe"
 
 REM Chemin vers le fichier .class de WorkerSocket
 set CLASS_PATH="C:\Users\IAN78\Desktop\Dufaud\out\production\Dufaud"
@@ -13,7 +15,7 @@ set NUM_WORKERS=12
 REM Lancer les workers dans une boucle
 for /L %%i in (1,1,%NUM_WORKERS%) do (
     set /A PORT=%START_PORT% + %%i - 1
-    start "Worker %%i" %JAVA_EXE% -cp %CLASS_PATH% WorkerSocket !PORT!
+    start "Worker %%i" /B %JAVA_EXE% -cp %CLASS_PATH% WorkerSocket !PORT!
 )
 
 echo Tous les workers ont été lancés.
